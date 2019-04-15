7bf8fe23b5c3d7b23315008c98f176ff28bab700
Nilton OS <nioliveira@equinix.com>
2019-04-15 18:33:25 -0300

    Update build

--- build.sh
+++ build.sh
@@ -5,21 +5,21 @@ rm -rf build/
 
 ## for Mac
 (
-    GOOS=darwin GOARCH=amd64 go build -o build/darwin-amd64/pw cmd/pw/*.go
+    GOOS=darwin GOARCH=amd64 GO111MODULE=on go build -ldflags "-s -w" -v -o build/darwin-amd64/pwgen cmd/pw/*.go
     cd ./build/darwin-amd64 || exit
-    tar cfz pw-darwin-amd64.tar.gz pw
+    tar cfz pw-darwin-amd64.tar.gz pwgen
 )
 
 ## for windows
 (
-    GOOS=windows GOARCH=amd64 go build -o build/windows-amd64/pw.exe cmd/pw/*.go
+    GOOS=windows GOARCH=amd64 GO111MODULE=on go build -ldflags "-s -w" -v -o build/windows-amd64/pwgen.exe cmd/pw/*.go
     cd ./build/windows-amd64 || exit
-    zip -q pw-windows-amd64.tar.gz pw.exe
+    zip -q pw-windows-amd64.tar.gz pwgen.exe
 )
 
 ## for linux
 (
-    GOOS=linux GOARCH=amd64 go build -o build/linux-amd64/pw cmd/pw/*.go
+    GOOS=linux GOARCH=amd64 GO111MODULE=on go build -ldflags "-s -w" -v -o build/linux-amd64/pwgen cmd/pw/*.go
     cd ./build/linux-amd64 || exit
-    tar cfz pw-linux-amd64.tar.gz pw
+    tar cfz pw-linux-amd64.tar.gz pwgen
 )
