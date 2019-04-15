de97bca0f8baa0cb464bae95ed382a8c8dfa63d2
Nilton OS <nioliveira@equinix.com>
2019-04-15 18:34:08 -0300

    Update build

--- build.sh
+++ build.sh
@@ -7,19 +7,19 @@ rm -rf build/
 (
     GOOS=darwin GOARCH=amd64 GO111MODULE=on go build -ldflags "-s -w" -v -o build/darwin-amd64/pwgen cmd/pw/*.go
     cd ./build/darwin-amd64 || exit
-    tar cfz pw-darwin-amd64.tar.gz pwgen
+    tar cfz pwgen-darwin-amd64.tar.gz pwgen
 )
 
 ## for windows
 (
     GOOS=windows GOARCH=amd64 GO111MODULE=on go build -ldflags "-s -w" -v -o build/windows-amd64/pwgen.exe cmd/pw/*.go
     cd ./build/windows-amd64 || exit
-    zip -q pw-windows-amd64.tar.gz pwgen.exe
+    zip -q pwgen-windows-amd64.tar.gz pwgen.exe
 )
 
 ## for linux
 (
     GOOS=linux GOARCH=amd64 GO111MODULE=on go build -ldflags "-s -w" -v -o build/linux-amd64/pwgen cmd/pw/*.go
     cd ./build/linux-amd64 || exit
-    tar cfz pw-linux-amd64.tar.gz pwgen
+    tar cfz pwgen-linux-amd64.tar.gz pwgen
 )
